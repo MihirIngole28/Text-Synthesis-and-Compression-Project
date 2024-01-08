cfd0e3e837162fbe5afcdf717eacf78819db184b
MihirIngole28 <myingole28@gmail.com>
2024-01-08 22:36:37 +0000

    Data Transformation Stage Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSynthesisAndCompression.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSynthesisAndCompression.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.textSynthesisAndCompression.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.textSynthesisAndCompression.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -17,6 +18,16 @@ try:
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<\n\n")
 except Exception as e:
     logger.exception(e)
